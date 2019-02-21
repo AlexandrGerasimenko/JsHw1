495464292717126dc7d0bc402fe0c0b607fb8895
Aleksandr Gerasimenko <avtoelectronic1987@gmail.com>
2019-02-22 01:07:15 +0200

    2

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+// Задача 1 тернарный оператор
+var x;
+x>0 ? console.log("x больше 0") : console.log("x меньше 0")
+
+
+// Задача 1 if
+if (x>0) {
+    console.log("x больше 0")
+} else {
+    console.log("x меньше 0")
+}
+
+
+// Задача 2
+
+var res=0;
+var z=0;
+for (i=0; i<10; i++){
+    res+=z+=5
+} console.log(res)
+
+// Задача 3
+
+var numbers = [ 254, 115, 78, 25, 91, 45, 37 ]
+for ( i = 0; i < numbers.length; i++ ) {
+    if (numbers[i]>50) 
+console.log (numbers[i])
+}
